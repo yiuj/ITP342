3bb0f8ab9b902fac8ecc3799e66fd55ddc4cfa6c
Student <student@KAP160-01M.local>
2018-08-22 12:39:26 -0700

    First project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Student on 8/22/18.
+//  Copyright © 2018 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
