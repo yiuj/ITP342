b3594aa04d7b0561e5cd846e7b899865a0a7125f
Student <student@usc-guestwireless-upc-newsc11697.usc.edu>
2018-09-05 12:49:27 -0700

    interactive

---
+++ Week3/Week3/StartViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StartViewController.swift
+//  Week3
+//
+//  Created by Student on 9/5/18.
+//  Copyright © 2018 yiuj. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
