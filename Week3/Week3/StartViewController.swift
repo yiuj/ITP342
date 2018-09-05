910a53fde44adc25dd8234964da7a0792ff467d7
Student <student@usc-guestwireless-upc-newsc11697.usc.edu>
2018-09-05 13:46:37 -0700

    more examples

--- Week3/Week3/StartViewController.swift
+++ Week3/Week3/StartViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    // Class attributes - variables
+    @IBOutlet weak var nameTF: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if (self.nameTF.text?.count)! > 0 {
+            self.messageLabel.text = "Fight on, \(self.nameTF.text)!"
+        } else {
+            messageLabel.text = "Fight On!"
+        }
+    }
+    // Class methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+//    @IBAction func textfieldDone(_ sender: UITextField) {
+//        sender.resignFirstResponder()
+//    }
 
+    @IBAction func backgroundButtonTouched(_ sender: UIButton) {
+        self.nameTF.resignFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
