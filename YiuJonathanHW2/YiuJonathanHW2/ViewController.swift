a24334442c8f221ebcb72c53fd142b6be88ed319
Jonathan Yiu <jonathanyiu@MyMacs-MBP.lan1>
2018-09-10 17:20:55 -0700

    starting hw2

---
+++ YiuJonathanHW2/YiuJonathanHW2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YiuJonathanHW2
+//
+//  Created by Jonathan Yiu on 9/10/18.
+//  Copyright © 2018 yiuj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
