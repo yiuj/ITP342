96ebe23af57757a82e1de23bcee374de9770693b
Jonathan Yiu <jonathanyiu@MyMacs-MBP.lan1>
2018-09-28 14:28:29 -0700

    finished tip, subtotal, and tax

--- YiuJonathanHW3/YiuJonathanHW3/ViewController.swift
+++ YiuJonathanHW3/YiuJonathanHW3/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var includeTaxSwitch: UISwitch!
     
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    
     @IBOutlet weak var tipSlider: UISlider!
     
     @IBOutlet weak var splitLabel: UILabel!
@@ -36,9 +38,27 @@ class ViewController: UIViewController {
     
     var tax: Float! = 0.075
     var subtotal: Float! = 0.0
+    var tipPercent: Float! = 0.15
     var tip: Float! = 0.0
     var totalWTip: Float! = 0.0
     var totalPPerson: Float! = 0.0
+    var includeTax: Bool! = false;
+    
+    @IBAction func taxPercentChanged(_ sender: UISegmentedControl) {
+        updateValues()
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        includeTax = !includeTax
+        updateValues()
+    }
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let numberAsInt = Int(sender.value + 0.5)
+        self.tipPercentLabel.text = "\(numberAsInt)%"
+        tipPercent = Float(numberAsInt) * 0.01
+        updateValues()
+    }
     
     @IBAction func backgroundButtonExit(_ sender: UIButton) {
         self.numberTextField.resignFirstResponder()
@@ -51,9 +71,29 @@ class ViewController: UIViewController {
     }
     
     func updateValues() {
+        var billText = numberTextField.text
+        if billText!.isEmpty {
+            billText = "0"
+        }
+        let bill = Float(billText!)
+        
+        //updating tax
         let index = self.taxSegmented.selectedSegmentIndex
-        tax = (0.075 + (Float(index) * 0.005)) * Float(numberTextField.text!)!
+        tax = (0.075 + (Float(index) * 0.005)) * bill!
         self.taxLabel.text = "$\(String(format: "%.2f", tax!))"
+        
+        //updating subtotal
+        if(includeTax) {
+            subtotal = bill! + tax
+        }
+        else {
+            subtotal = bill!
+        }
+        self.subtotalLabel.text = "$\(String(format: "%.2f", subtotal!))"
+        
+        //updating tip
+        tip = tipPercent * subtotal!
+        self.tipLabel.text = "$\(String(format: "%.2f", tip!))"
     }
     
     override func viewDidLoad() {
