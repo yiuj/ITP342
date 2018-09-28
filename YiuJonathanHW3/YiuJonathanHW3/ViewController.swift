be8949c11bacea75240ef568cc437b7e80f08b5f
Jonathan Yiu <jonathanyiu@MyMacs-MBP.lan1>
2018-09-28 13:48:22 -0700

    tax working

--- YiuJonathanHW3/YiuJonathanHW3/ViewController.swift
+++ YiuJonathanHW3/YiuJonathanHW3/ViewController.swift
@@ -10,6 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var taxSegmented: UISegmentedControl!
+    
+    @IBOutlet weak var includeTaxSwitch: UISwitch!
+    
+    @IBOutlet weak var tipSlider: UISlider!
+    
+    @IBOutlet weak var splitLabel: UILabel!
+    
+    @IBOutlet weak var splitStepper: UIStepper!
+    
+    @IBOutlet weak var taxLabel: UILabel!
+    
+    @IBOutlet weak var subtotalLabel: UILabel!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalWithTipLabel: UILabel!
+    
+    @IBOutlet weak var totalPerPersonLabel: UILabel!
+    
+    @IBOutlet weak var clearAllButton: UIButton!
+    
+    var tax: Float! = 0.075
+    var subtotal: Float! = 0.0
+    var tip: Float! = 0.0
+    var totalWTip: Float! = 0.0
+    var totalPPerson: Float! = 0.0
+    
+    @IBAction func backgroundButtonExit(_ sender: UIButton) {
+        self.numberTextField.resignFirstResponder()
+        updateValues()
+    }
+    
+    @IBAction func textFieldExit(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        updateValues()
+    }
+    
+    func updateValues() {
+        let index = self.taxSegmented.selectedSegmentIndex
+        tax = (0.075 + (Float(index) * 0.005)) * Float(numberTextField.text!)!
+        self.taxLabel.text = "$\(String(format: "%.2f", tax!))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
