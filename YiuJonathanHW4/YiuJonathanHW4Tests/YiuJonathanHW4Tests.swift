51c9b13a20c2ce4a3d7aff2cd419a969136e8e03
Jonathan Yiu <jonathanyiu@MyMacs-MBP.lan1>
2018-10-19 04:14:31 -0700

    starting hw4

---
+++ YiuJonathanHW4/YiuJonathanHW4Tests/YiuJonathanHW4Tests.swift
@@ -0,0 +1,36 @@
+//
+//  YiuJonathanHW4Tests.swift
+//  YiuJonathanHW4Tests
+//
+//  Created by Jonathan Yiu on 10/19/18.
+//  Copyright © 2018 yiuj. All rights reserved.
+//
+
+import XCTest
+@testable import YiuJonathanHW4
+
+class YiuJonathanHW4Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
